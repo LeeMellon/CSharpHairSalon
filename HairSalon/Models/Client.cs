3a0fb3b2d8dabbd5644156a96839cb1ca78424d2
Ian Goodrich <iangoodrich@hotmail.com>
2018-02-23 11:02:23 -0800

    all classes, getters and setters, and equality overrides added.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,6 +7,106 @@ namespace HairSalon.Models
 {
     public class Client
     {
+      private int _id;
+      private string _nameFirst;
+      private string _nameLast;
+      private int _number;
+      private string _email;
+      private int _stylistId;
+
+      public Client (string firstName, string lastName, int number, string email, int stylistId, int id = 0)
+      {
+        _id = id;
+        _nameFirst = firstName;
+        _nameLast = lastName;
+        _number = number;
+        _email = email;
+        _stylistId = stylistId;
+      }
+
+      public void SetFirstName(string firstName)
+      {
+        _nameFirst = firstName;
+      }
+
+      public string GetFirstName()
+      {
+        return _nameFirst;
+      }
+
+      public void SetLastName(string lastName)
+      {
+        _nameLast = lastName;
+      }
+
+      public string GetLastName()
+      {
+        return _nameLast;
+      }
+
+      public void SetNumber(int number)
+      {
+        _number = number;
+      }
+
+      public int GetNumber()
+      {
+        return _number;
+      }
+
+      public void SetEmail(string email)
+      {
+        _email = email;
+      }
+
+      public string GetEmail()
+      {
+        return _email;
+      }
+
+      public void SetStylistId(int stylist_Id)
+      {
+        _stylistId = stylistId;
+      }
+
+      public int GetStylistId()
+      {
+        return _stylistId;
+      }
+
+      public void SetId(int id)
+      {
+        _id = id;
+      }
+
+      public int GetId()
+      {
+        return _id;
+      }
+
+      public override bool Equals(System.Object otherClient)
+      {
+        if (!(otherClient is Client))
+        {
+          return false;
+        }
+        else
+        {
+          Client newClient = (Client) otherClient;
+          bool idEquality = (this.GetId() == newClient.GetId());
+          bool firstNameEquality = (this.GetFirstName() == newClient.GetFirstName());
+          bool lastNameEquality = (this.GetLastName() == newClient.GetLastName());
+          bool numberEquality = (this.GetNumber() == newClient.GetNumber());
+          bool emailEquality = (this.GetEmail() == newClient.GetEmail());
+          bool stylistIdEquality = (this.GetStylistId() == newClient.GetStylistId());
+          return (idEquality && firstNameEquality && lastNameEquality && numberEquality && emailEquality && stylistIdEquality);
+        }
+      }
+
+      public override int GetHashCode()
+      {
+        return this.GetId().GetHashCode();
+      }
 
 
     }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,7 +7,68 @@ namespace HairSalon.Models
 {
     public class Stylist
     {
+      private int _id;
+      private string _name;
+      private int _chair;
 
+
+      public Stylist (string name, int chair, int id = 0)
+      {
+        _id = id;
+        _name = name;
+        _chair = chair;
+      }
+
+      public void SetStylistName(string name)
+      {
+        _name = name;
+      }
+
+      public string GetStylistName()
+      {
+        return _name;
+      }
+
+      public void SetStylistChair(int chair)
+      {
+        _chair = chair;
+      }
+
+      public int GetStylstChair()
+      {
+        return _chair;
+      }
+
+      public void StylistSetId(int id)
+      {
+        _id = id;
+      }
+
+      public int StylistGetId()
+      {
+        return _id;
+      }
+
+      public override bool Equals(System.Object otherStylist)
+      {
+        if (!(otherStylist is Stylist))
+        {
+          return false;
+        }
+        else
+        {
+          Stylist newStylist = (Stylist) otherStylist;
+          bool nameEquality = (this.GetStylistName() == newStylist.GetStylistName());
+          bool idEquality = (this.StylistGetId() == newStylist.StylistGetId());
+          bool chairEquality = (this.GetStylstChair() == newStylist.GetStylstChair());
+          return (idEquality && nameEquality && chairEquality);
+        }
+      }
+
+      public override int GetHashCode()
+      {
+        return this.GetId().GetHashCode();
+      }
 
     }
 
